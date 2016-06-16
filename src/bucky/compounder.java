3db1ab40009ffd33c2bd7ff0edab217fdbaa6776
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-16 21:39:03 +0100

    Modified

--- src/bucky/compounder.java
+++ src/bucky/compounder.java
@@ -2,6 +2,19 @@ package bucky;
 
 public class compounder {
 	public static void main(String[] args){
-		
+		/*
+		 * A = Amount
+		 * P = Principle
+		 * R = Rate
+		 * n = number of years
+		 * A=P(1+R)^n
+		 */
+		double amount;
+		double principal = 10000;
+		double rate = .01;
+		for(int year=1;year<=20;year++){
+			amount = principal*Math.pow(1+rate, year);
+			System.out.println(year + "  " + amount);
+		}
 	}
 }
