51a7cec48203cb2a110dcbd63c18ade754ca8b2b
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-16 18:31:24 +0100

    Modified

--- src/bucky/apples.java
+++ src/bucky/apples.java
@@ -7,9 +7,18 @@ class apples {
 		// System.out.println("Hello youtube!");
 		double tuna;
 		tuna = 5.28;
-		System.out.print(tuna);
-		
+		// System.out.print(tuna);
+		/* 
+		 * Calculator
+		 */
 		Scanner bucky = new Scanner(System.in);
-		System.out.println(bucky.nextLine());
+		double fnum, snum, answer;
+		// System.out.println(bucky.nextLine());
+		System.out.println("Enter first num: ");
+		fnum = bucky.nextDouble();
+		System.out.println("Enter second num: ");
+		snum = bucky.nextDouble();
+		answer = fnum + snum;
+		System.out.println(answer);
 	}
 }
