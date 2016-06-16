3a9921fd99db397242cd725b779d6b6b15af77e9
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-16 21:12:15 +0100

    Modified

---
+++ src/bucky/grader.java
@@ -0,0 +1,19 @@
+package bucky;
+
+import java.util.Scanner;
+
+public class grader {
+	public static void main (String[] args){
+		  Scanner input = new Scanner(System.in);
+		  int total = 0;
+		  int grade;
+		  int average;
+		  int counter = 0;
+		  while (counter < 10){
+			  grade = input.nextInt();
+			  total = total + grade;
+			  counter++;
+		  }
+		  average = total/10;
+	}
+}
--- src/bucky/person.java
+++ src/bucky/person.java
@@ -2,6 +2,12 @@ package bucky;
 
 public class person {
 	private String personName;
+	
+	// A constructor has the same name as the class
+	public void person(String name){
+		personName = name;
+	}
+	
 	// If a method does not return anything, use void
 	public void setName(String name){
 		personName = name;
