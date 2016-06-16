e10c4aca49879b630d987aab734a18f4ce4ef6a3
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-16 14:31:12 +0100

    Modified

--- youtube.java
+++ youtube.java
@@ -1,3 +1,11 @@
+/*
+ * File: youtube.java
+ * Compile: javac youtube.java
+ * Run: java apples
+ */
+
 class apples {
-	
+	public static void main(String args[]) {
+		System.out.println("Hello youtube!");
+	}
 }
