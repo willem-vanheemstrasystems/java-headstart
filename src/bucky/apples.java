275fa56a484b1f3e796df77387d4ff7fff267458
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-16 18:10:23 +0100

    Modified

--- src/bucky/apples.java
+++ src/bucky/apples.java
@@ -1,7 +1,15 @@
 package bucky;
 
+import java.util.Scanner;
+
 class apples {
 	public static void main(String args[]) {
-		System.out.println("Hello youtube!");
+		// System.out.println("Hello youtube!");
+		double tuna;
+		tuna = 5.28;
+		System.out.print(tuna);
+		
+		Scanner bucky = new Scanner(System.in);
+		System.out.println(bucky.nextLine());
 	}
 }
