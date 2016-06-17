e848ea9fb87fff5f8cfb3d7dec1403aae75fd56c
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-17 20:23:49 +0100

    Modified

--- src/bucky/timecalculatormain.java
+++ src/bucky/timecalculatormain.java
@@ -4,5 +4,8 @@ public class timecalculatormain {
 	public static void main(String[] args){
 		timecalculator timecalculatorObject = new timecalculator();
 		System.out.println(timecalculatorObject.toMilitary());
+		timecalculatorObject.setTime(13, 27, 6);
+		System.out.println(timecalculatorObject.toMilitary());
+		System.out.println(timecalculatorObject.toNormal());
 	}
 }
