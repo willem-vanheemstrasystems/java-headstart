06c45bf52ff00ccbad28feba588056014d69e463
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-17 20:01:37 +0100

    Modified

---
+++ src/bucky/averager.java
@@ -0,0 +1,11 @@
+package bucky;
+
+public class averager {
+	public static void main(String[] args) {
+	}
+	public static int average(int...numbers) { // ... means an unknown number of input values
+		int total=0;
+		for(int x:numbers) // enhanced for loop
+			total+=x; // adds x to total, total becomes new total
+	}
+}
