0c7636a77f70ac483f6ee0cc05f7f9e6c91710de
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-19 14:46:24 +0100

    Modified

--- src/bucky/Club.java
+++ src/bucky/Club.java
@@ -12,4 +12,20 @@ public class Club {
 		members++; // Increases for each added member
 		System.out.printf("Constructor for %s %s, members in the club: %d.\n", first, last, members);
 	}
+	
+	// Method
+	public String getFirst(){
+		return first;
+	}
+	
+	// Method
+	public String getLast(){
+		return last;
+	}
+	
+	// Method, notice the static!
+	public static int getMembers(){
+		return members;
+	}
+	
 }
--- src/bucky/ClubMain.java
+++ src/bucky/ClubMain.java
@@ -5,5 +5,20 @@ public class ClubMain {
 		Club member1 = new Club("Megan", "Fox");
 		Club member2 = new Club("Natalie", "Portman");
 		Club member3 = new Club("Taylor", "Swift");
+		
+		System.out.println(); // Empty line
+		
+		// Member 1
+		System.out.println(member1.getFirst());
+		System.out.println(member1.getLast());
+		System.out.println(member1.getMembers());
+		
+		// Member 2
+		System.out.println(member2.getFirst());
+		System.out.println(member2.getLast());
+		System.out.println(member2.getMembers());
+		
+		// Static methods & variables return on the Class itself too
+		System.out.println(Club.getMembers());		
 	}
 }
