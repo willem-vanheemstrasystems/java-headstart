d1eba11d403480d4afbe3f35dc5b260a20db10cf
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-19 14:29:27 +0100

    Modified

--- src/bucky/ClubMain.java
+++ src/bucky/ClubMain.java
@@ -3,5 +3,7 @@ package bucky;
 public class ClubMain {
 	public static void main(String[] args){
 		Club member1 = new Club("Megan", "Fox");
+		Club member2 = new Club("Natalie", "Portman");
+		Club member3 = new Club("Taylor", "Swift");
 	}
 }
