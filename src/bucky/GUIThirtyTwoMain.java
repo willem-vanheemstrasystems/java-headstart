95463bb04a9536509cae5143aa1d64c910fc8b4a
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-22 14:18:38 +0100

    Modified

--- src/bucky/GUIThirtyTwoMain.java
+++ src/bucky/GUIThirtyTwoMain.java
@@ -5,6 +5,17 @@ import java.util.*;
 public class GUIThirtyTwoMain {
 	public static void main(String[] args){
 		// Variables
+		String[] fruitsArray = {"apple", "pear", "cherry", "banana"};
+		List<String> fruitsList = Arrays.asList(fruitsArray);
+		// Create an ArrayList
+		// Note: an ArrayList can change in size
+		ArrayList<String> arrayList = new ArrayList<String>();
+		arrayList.add("sweet");
+		arrayList.add("savory");
+		arrayList.add("sour");
+		// Enhanced for loop
+		for(String flavour: arrayList.toArray())
+			
 		
 		
 		
--- src/bucky/STREAMOneMain.java
+++ src/bucky/STREAMOneMain.java
@@ -1,21 +1,21 @@
 package bucky;
 
 import java.util.*;
+import static java.util.stream.Collectors.*;
 
 public class STREAMOneMain {
 	public static void main(String[] args){
 		// Variables
-		int e;
+
 		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 		// Multiply each number by two using streams
 		// A Stream is a more powerful Iterator
 		List<Integer> results = 
 			numbers.stream()
-			//	.filter(e - e % 2 == 0) // Filter for only even numbers, using modulo %
+				.filter(element -> element % 2 == 0) // Filter for only even numbers, using modulo %
 				.map(element -> element * 2) // Double the element
 				.collect(toList()); // Put the result into a collection
 			
-		
 		// Convert List to Array
 		results.toArray();
 		// Enhanced for loop
