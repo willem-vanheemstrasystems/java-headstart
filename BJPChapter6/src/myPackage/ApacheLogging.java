5a584a6d6ba8dfdee2cbc4960d9c66ba679bf1c3
wvanheemstra <willem@vanheemstrasystems.com>
2016-06-27 15:58:18 +0100

    Modified

--- BJPChapter6/src/myPackage/ApacheLogging.java
+++ BJPChapter6/src/myPackage/ApacheLogging.java
@@ -3,16 +3,39 @@
  */
 package myPackage;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 /**
- * @author user
- *
+ * This class uses Apache Log4j 2
+ * Download it from
+ * http://logging.apache.org/log4j/2.x/index.html
+ * Extract the zip file and add the following jar files to the Referenced Libraries
+ * - log4j-api-2.x.x.jar
+ * - log4j-core-2.x.x.jar
  */
 public class ApacheLogging {
-	/**
-	 * @param args
-	 */
+	// Class variable
+	static final Logger logger = LogManager.getLogger(ApacheLogging.class.getName());
+	// Method main
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Variables
+		int age = 30;
+		double retirementFund = 10000;
+		int yearsInRetirement = 20;
+		String name = "David Johnson";
+		// For loop
+		for(int i = age; i<=65; i++ ){
+			recalculate(retirementFund, 0.1);
+		}
+		double monthlyPension = retirementFund / yearsInRetirement / 12;
+		System.out.println(name + " will have $" + monthlyPension + " per month for retirement.");
+		if(monthlyPension < 100){
+			System.out.println("monthlyPension is too low.");
+		}
 	}
+	// Method recalculate
+	public static void recalculate(double fundAmount, double rate){
+		fundAmount = fundAmount * (1 + rate);
+	} 
 }
